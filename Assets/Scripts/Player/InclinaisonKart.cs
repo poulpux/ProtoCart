0c487161562974f2e5b9c5fbd190e35bb343b533
Poulpux <114729806+poulpux@users.noreply.github.com>
2024-04-03 15:16:45 +0200

    Save avant la fin

--- Assets/Scripts/Player/InclinaisonKart.cs
+++ Assets/Scripts/Player/InclinaisonKart.cs
@@ -9,39 +9,30 @@ public class InclinaisonKart : MonoBehaviour
 
     void Update()
     {
-        RaycastHit hit1, hit2, hit3;
+        transform.rotation = Quaternion.Slerp(transform.rotation, CalculateAngle(), rotationSpeed * Time.deltaTime);
+    }
+
+    private Quaternion CalculateAngle()
+    {
         float dist1 = 0f, dist2 = 0f, dist3 = 0f;
-        Physics.Raycast(P1.transform.position, Vector3.down, out hit1, 1f);
-        Physics.Raycast(P2.transform.position, Vector3.down, out hit2, 1f);
-        Physics.Raycast(P3.transform.position, Vector3.down, out hit3, 1f);
 
-            if (hit1.collider != null)
-                dist1 = hit1.distance-0.5f;
-            else dist1 = 1f;
-            if (hit2.collider != null)
-                dist2 = hit2.distance - 0.5f;
-            else
-                dist2 = 1f;
-            if (hit3.collider != null)
-                dist3 = hit3.distance -0.5f ;
-            else
-                dist3 = 1f;
+        dist1 = RaycastDist(1);
+        dist2 = RaycastDist(2);
+        dist3 = -RaycastDist(3);
 
-        dist3 = -dist3;
-        float eulerX =/* (dist2 + dist1 + dist3)*/ dist1 + dist3 ;
-
-        //if (dist1 > dist2 && dist2 > dist3)
-        //    eulerX = 25;
-
-        //if (dist1 < dist2 && dist2 < dist3)
-        //    eulerX = 25;
-
+        float eulerX = dist1 + dist3;
         if (dist2 == 1f)
             eulerX = 0f;
-        print(eulerX);
-        //if(eulerX < 0f) 
-        //    print(eulerX);
-        Quaternion angle = Quaternion.Euler(new Vector3(eulerX * 20f , transform.eulerAngles.y, transform.eulerAngles.z));
-        transform.rotation = Quaternion.Slerp(transform.rotation, angle, rotationSpeed * Time.deltaTime);
+
+        return Quaternion.Euler(new Vector3(eulerX * 20f, transform.eulerAngles.y, transform.eulerAngles.z));
+    }
+
+    private float RaycastDist(int nb)
+    {
+        RaycastHit hit;
+        GameObject P = nb == 1 ? P1 : nb == 2 ? P2 : P3;
+        Physics.Raycast(P.transform.position, Vector3.down, out hit, 1f);
+
+        return hit.collider != null ? hit.distance - 0.5f : 1f;
     }
 }
